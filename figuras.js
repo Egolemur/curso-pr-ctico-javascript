cddd9ad3dba5b0be4ce44a85392c80e0ffa92bc9
Leonardo Murillo <egolemur@gmail.com>
2022-02-06 21:12:33 -0800

    Creación de funciones para encapsular código de las figuras geométricas

--- figuras.js
+++ figuras.js
@@ -1,1 +1,48 @@
-console.log ("Anita la huerfanita")
+// Código del cuadrado
+
+console.group("cuadrado");
+
+function perimetroCuadrado(lado) {
+    return lado * 4;
+}
+
+function areaCuadrado(lado) {
+    return lado * lado;
+}
+
+console.groupEnd(); 
+
+// Código del triángulo
+
+console.group("triángulo");
+
+function perimetroTriangulo(lado1, lado2, base) {
+    return lado1 + lado2 + base;
+}
+
+function areaTriangulo(base, altura) {
+    return (base * altura) / 2;
+}
+
+console.groupEnd();
+
+// Código del círculo
+
+console.group("círculo");
+
+const pi = Math.PI;
+
+function diametroCirculo(radio) {
+    return radio * 2;
+}
+
+function circunferencia(radio) {
+    const diametro = diametroCirculo(radio);
+    return diametro * pi;
+}
+
+function areaCirculo(radio) {
+    return (radio * radio) * pi;
+} 
+
+console.groupEnd();
